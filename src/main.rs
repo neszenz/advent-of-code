63323564b1b93d51c2b732cc587ba7dfd4af6101
Tobias Newe <tobias.newe@t-online.de>
2023-12-03 21:56:03 +0100

    Solve day 1 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+const RESOURCE_FILE_PATH: &str = "res/input";
+
 fn main() {
-    println!("Hello, world!");
+    let input = std::fs::read_to_string(RESOURCE_FILE_PATH).expect("resource file can be loaded");
+
+    let result: i32 = input
+        .lines()
+        .map(|line| {
+            line
+                .chars()
+                .filter(|char| char.is_numeric())
+                .collect::<String>()
+        })
+        .map(|filtered| {
+            let fd: char = filtered.chars().next().unwrap();
+            let ld: char = filtered.chars().last().unwrap();
+            (fd.to_string() + &ld.to_string()).parse::<i32>().unwrap()
+        })
+        .sum();
+
+    println!("result={}", result);
 }
