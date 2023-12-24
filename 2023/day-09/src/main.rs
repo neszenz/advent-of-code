19371f8172125fba240f1c43c12fceb92da320f4
Tobias Newe <tobias.newe@t-online.de>
2023-12-24 16:56:24 +0100

    Solve day 9 part 2

--- 2023/day-09/src/main.rs
+++ 2023/day-09/src/main.rs
@@ -1,24 +1,49 @@
 #![allow(dead_code)]
 
+fn compute_derivative(values: &Vec<i64>) -> Vec<i64> {
+    values
+        .windows(2)
+        .map(|w| w[1] - w[0])
+        .collect()
+}
+
+fn compute_all_derivatives(values: &Vec<i64>) -> Vec<Vec<i64>> {
+    let mut derivatives: Vec<Vec<i64>> = Vec::from([compute_derivative(values)]);
+
+    while derivatives.last().unwrap().iter().any(|ele| *ele != 0) {
+        derivatives.push(compute_derivative(derivatives.last().unwrap()));
+    }
+
+    derivatives
+}
+
 #[derive(Debug)]
 struct History (Vec<i64>);
 
 impl History {
     fn predict_next(self: &Self) -> i64 {
-        fn compute_derivative(values: &Vec<i64>) -> Vec<i64> {
-            values
-                .windows(2)
-                .map(|w| w[1] - w[0])
-                .collect()
-        }
-
-        let mut derivatives: Vec<Vec<i64>> = Vec::from([compute_derivative(&self.0)]);
-
-        while derivatives.last().unwrap().iter().any(|ele| *ele != 0) {
-            derivatives.push(compute_derivative(derivatives.last().unwrap()));
-        }
-
+        let derivatives = compute_all_derivatives(&self.0);
         let prediction = self.0.last().unwrap() + derivatives.iter().map(|d| d.last().unwrap()).sum::<i64>();
+
+        prediction
+    }
+
+    fn extrapolate_backwards(self: &Self) -> i64 {
+        let derivatives = compute_all_derivatives(&self.0);
+
+        let mut val = 0;
+        (0..derivatives.len())
+            .rev()
+            .for_each(|i| {
+                if i + 1 == derivatives.len() {
+                    val = *derivatives[i].first().unwrap();
+                }
+                else {
+                    val = derivatives[i].first().unwrap() - val;
+                }
+            });
+
+        let prediction = self.0.first().unwrap() - val;
 
         prediction
     }
@@ -46,7 +71,7 @@ impl Oasis {
 }
 
 #[test]
-fn example() {
+fn example_part_1() {
     static INPUT: &str = include_str!("../res/example");
     static ANSWER: i64 = 114;
 
@@ -60,6 +85,21 @@ fn example() {
     assert_eq!(result, ANSWER);
 }
 
+#[test]
+fn example_part_2() {
+    static INPUT: &str = include_str!("../res/example");
+    static ANSWER: i64 = 2;
+
+    let oasis = Oasis::parse(INPUT);
+
+    let result: i64 = oasis.0
+        .iter()
+        .map(|h| h.extrapolate_backwards())
+        .sum();
+
+    assert_eq!(result, ANSWER);
+}
+
 fn main() {
     static INPUT: &str = include_str!("../res/input");
 
@@ -67,7 +107,7 @@ fn main() {
 
     let result: i64 = oasis.0
         .iter()
-        .map(|h| h.predict_next())
+        .map(|h| h.extrapolate_backwards())
         .sum();
     println!("result={result}");
 }
