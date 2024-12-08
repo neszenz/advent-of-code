302942b22a11a78ec809c214dad31e729aa357a6
Tobias Newe <tobias.newe@t-online.de>
2024-12-08 15:48:52 +0100

    Solve day 1 part 2

--- 2024/day01/src/main.rs
+++ 2024/day01/src/main.rs
@@ -1,5 +1,7 @@
-fn solve(input: &str) -> i32 {
-    let pared_input : Vec<(i32, i32)> = input
+use std::collections::HashMap;
+
+fn parse_input(input: &str) -> (Vec<i32>, Vec<i32>) {
+    let parsed_input : Vec<(i32, i32)> = input
         .lines()
         .map(|l| l.split(' ').collect::<Vec<&str>>())
         .map(|v| {
@@ -15,7 +17,7 @@ fn solve(input: &str) -> i32 {
     ;
 
     let list_0 : Vec<i32> = {
-        let mut list = pared_input
+        let mut list = parsed_input
             .iter()
             .map(|pair| pair.0)
             .collect::<Vec<i32>>()
@@ -27,7 +29,7 @@ fn solve(input: &str) -> i32 {
     };
 
     let list_1 : Vec<i32> = {
-        let mut list = pared_input
+        let mut list = parsed_input
             .iter()
             .map(|pair| pair.1)
             .collect::<Vec<i32>>()
@@ -39,6 +41,12 @@ fn solve(input: &str) -> i32 {
     };
 
     assert_eq!(list_0.len(), list_1.len());
+
+    (list_0, list_1)
+}
+
+fn solve_part_1(input: &str) -> i32 {
+    let (list_0, list_1) = parse_input(input);
 
     let distances : Vec<i32> = (0..list_0.len())
         .map(|i| (list_0.get(i).unwrap() - list_1.get(i).unwrap()).abs())
@@ -53,13 +61,50 @@ fn solve(input: &str) -> i32 {
     return total_distance;
 }
 
+fn solve_part_2(input: &str) -> i32 {
+    let (list_0, list_1) = parse_input(input);
+
+    fn count_id_occurrences(list: &Vec<i32>) -> HashMap<i32, i32> {
+        let mut map = HashMap::new();
+
+        for value in list {
+            map
+                .entry(*value)
+                .and_modify(|v| *v += 1)
+                .or_insert(1)
+            ;
+        }
+
+        map
+    }
+
+    let map_1 : HashMap<i32, i32> = count_id_occurrences(&list_1);
+
+    let result : i32 = list_0
+        .iter()
+        .map(|v| {
+            v * map_1.get(v).unwrap_or(&0)
+        })
+        .sum()
+    ;
+
+    result
+}
+
 #[test]
-fn example() {
-    let result = solve(include_str!("../res/example"));
+fn example_part_1() {
+    let result = solve_part_1(include_str!("../res/example"));
     assert_eq!(result, 11);
 }
 
+#[test]
+fn example_part_2() {
+    let result = solve_part_2(include_str!("../res/example"));
+    assert_eq!(result, 31);
+}
+
 fn main() {
-    let result = solve(include_str!("../res/input"));
-    println!("result={}", result);
+    let result_part_1 = solve_part_1(include_str!("../res/input"));
+    let result_part_2 = solve_part_2(include_str!("../res/input"));
+    println!("result_part_1={} result_part_2={}", result_part_1, result_part_2);
 }
